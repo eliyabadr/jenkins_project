8fcbd2459f2009e01a33175cda193e23cda3e000
eliya <eliyabadr2@gmail.com>
2025-11-10 23:41:31 +0200

    '1'

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 def greet(name):
     print(f"Hello {name}!")
 
+
 if __name__ == "__main__":
     print(greet("World"))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 import unittest
 from app import greet
 
+
 class TestApp(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World from Eliya Badr!")
